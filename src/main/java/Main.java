7cdab14d2e3a2566e8505c1041c3ffb324b08b03
imontecalvo <imontecalvo@fi.uba.ar>
2024-04-07 23:56:12 -0300

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
@@ -21,14 +19,16 @@ public class Main {
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
 
-          DataInputStream input = new DataInputStream(clientSocket.getInputStream());
-          DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+          BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          OutputStream output = clientSocket.getOutputStream();
 
-          byte[] r = new byte[10];
-          input.read(r);
-
-          String response = "+PONG\r\n";
-          output.write(response.getBytes());
+          while (!clientSocket.isClosed()){
+              char[] r = new char[100];
+              if (br.read(r) > 0){
+                  String response = "+PONG\r\n";
+                  output.write(response.getBytes());
+              }
+          }
 
       } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
