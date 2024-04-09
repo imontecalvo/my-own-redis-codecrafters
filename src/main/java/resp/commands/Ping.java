569cb6e5ea0856119bac51d5bc57a9bd7d06da2c
imontecalvo <imontecalvo@fi.uba.ar>
2024-04-09 16:47:09 -0300

    Fix ping command

--- src/main/java/resp/commands/Ping.java
+++ src/main/java/resp/commands/Ping.java
@@ -6,7 +6,7 @@ import resp.data_types.RedisString;
 public class Ping implements Command{
     @Override
     public byte[] execute() {
-        RedisString response = new RedisString("+PONG");
+        RedisString response = new RedisString("PONG");
         return response.toBytes();
     }
 }
