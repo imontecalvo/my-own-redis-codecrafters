92ef7d7ef75b216ca86a52350e2a010f67d28c15
imontecalvo <imontecalvo@fi.uba.ar>
2024-04-10 16:40:29 -0300

    8th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,33 @@ import java.net.Socket;
 import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        int port = 6379;
+        if (args.length == 2 && args[0].equals("--port")){
+            port = Integer.parseInt(args[1]);
+        }
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        {
+            System.out.println("Logs from your program will appear here!");
+        }
 
-      ServerSocket serverSocket = null;
-      Socket clientSocket = null;
-      int port = 6379;
-      try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
 
-          while(true){
-              clientSocket = serverSocket.accept();
+            while (true) {
+                clientSocket = serverSocket.accept();
 
-              Thread thread = new Thread(new ClientHandler(clientSocket));
-              thread.start();
-          }
-      } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-      }
-  }
+                Thread thread = new Thread(new ClientHandler(clientSocket));
+                thread.start();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
