63d1f5f87a9e08e0409fa083e3f55b7d2bcb30ec
imontecalvo <imontecalvo@fi.uba.ar>
2024-04-11 00:22:13 -0300

    another fix

--- src/main/java/resp/Request.java
+++ src/main/java/resp/Request.java
@@ -19,30 +19,13 @@ public class Request {
     }
 
     public static Request fromBytes(BufferedReader reader) throws IOException {
-        char[] r = new char[1];
-
-        if (reader.read(r) > 0) {
-            return switch (r[0]) {
-                case '+' -> simpleRequest(RedisString.fromBytes(reader));
-                case '$' -> simpleRequest(RedisBulkString.fromBytes(reader));
-                case '*' -> compoundRequest(RedisArray.fromBytes(reader));
-                default -> null;
-            };
+        DataType recv = Parser.fromBytes(reader);
+        if (!(recv instanceof RedisArray arrayRequest)){
+            return null;
         }
-        return null;
-    }
 
-    public static Request simpleRequest(RedisString command){
-        return new Request(command.getContent(), null);
-    }
-
-    public static Request simpleRequest(RedisBulkString command){
-        return new Request(command.getContent(), null);
-    }
-
-    public static Request compoundRequest(RedisArray array){
-        String command = ((RedisBulkString) array.getElement(0)).getContent();
-        return new Request(command, array.slice(1,-1));
+        String command = ((RedisBulkString) arrayRequest.getElement(0)).getContent();
+        return new Request(command, arrayRequest.slice(1,-1));
     }
 
     public String getCommand() {
