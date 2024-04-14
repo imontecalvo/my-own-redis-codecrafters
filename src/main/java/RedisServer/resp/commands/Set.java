a253b0194676959f9ed0cbc741d2886fabf026eb
imontecalvo <imontecalvo@fi.uba.ar>
2024-04-14 01:54:29 -0300

    18th stage - fix

--- src/main/java/RedisServer/resp/commands/Set.java
+++ src/main/java/RedisServer/resp/commands/Set.java
@@ -4,6 +4,7 @@ import RedisServer.resp.Propagator;
 import RedisServer.resp.Request;
 import RedisServer.resp.Storage;
 import RedisServer.resp.data_types.DataType;
+import RedisServer.resp.data_types.RedisArray;
 import RedisServer.resp.data_types.RedisBulkString;
 import RedisServer.resp.data_types.RedisString;
 
@@ -22,10 +23,16 @@ public class Set implements Command{
     }
 
     public byte[] getMessage(){
+        RedisBulkString command = new RedisBulkString("SET");
+        RedisBulkString keyArg = new RedisBulkString(key);
+
         if (ttl.isEmpty()){
-            return String.format("$3\r\nset\r\n%s\r\n%s",key,value).getBytes();
+            return new RedisArray(new DataType[]{command,keyArg,value}).toBytes();
         }
-        return String.format("$5\r\nset\r\n%s\r\n%s\r\npx\r\n%d\r\n",key,value, ttl.get()).getBytes();
+
+        RedisBulkString pxArg = new RedisBulkString("PX");
+        RedisBulkString pxValue = new RedisBulkString(ttl.get().toString());
+        return new RedisArray(new DataType[]{command,keyArg,value,pxArg,pxValue}).toBytes();
     }
 
     public static Set fromRequest(Request request){
